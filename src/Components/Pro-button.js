f6e3e3c02606bea810516d926783ba7e1970474a
24LP4300 24LP4300 <24LP4300@G305-CC0063.local>
2024-08-21 13:16:34 +0800

    Pro shinechlev

---
+++ src/Components/Pro-button.js
@@ -0,0 +1,9 @@
+export default function Button() {
+  return (
+    <div>
+      <button className="bg-stone-900 text-white w-36 px-4 h-8 mr-10 ml-8 rounded mt-16">
+        Download CV
+      </button>
+    </div>
+  );
+}
---
+++ src/Components/Pro-header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="flex mt-16 gap-12 ml-auto ml-30">
+      <p>About</p>
+      <p>Work</p>
+      <p>Testimonials</p>
+      <p>Contact</p>
+    </div>
+  );
+}
