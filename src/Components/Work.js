35f57453203937b53d91df39a9461b4b5e53900c
24LP4300 24LP4300 <24LP4300@G305-CC0063.local>
2024-08-23 10:15:45 +0800

    port yanzalalaa

---
+++ src/Components/Work.js
@@ -0,0 +1,14 @@
+export default function Work() {
+  return (
+    <div className="m-auto max-w-7xl">
+      <div>
+        <p className=" mt-24 w-28 h-7 rounded-xl bg-slate-200 flex m-auto justify-center mb-4 ">
+          Work
+        </p>
+        <p className="text-xl text-center mb-12">
+          Some of the noteworthy projects I have built:{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,9 +20,10 @@ import Figma from "../Components/icons/Figma";
 import Cyress from "../Components/icons/Cyress";
 import Story from "../Components/icons/Story";
 import Git from "../Components/icons/Git";
-import Experience from "../Components/Experience"
+import Experience from "../Components/Experience";
 import Experience2 from "../Components/Experience2";
-
+import Work from "../Components/Work";
+import Workscript from "../Components/Workscript";
 
 const datas = [
   {
@@ -106,31 +107,28 @@ const datas = [
     name: "Git",
   },
 ];
-const experience= [
+const experience = [
   {
-      head:" .SrFrontend Developer",
-      li1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      li2: "Ut pretium arcu et massa semper, id fringilla leo semper.",
-      li3: "Sed quis justo ac magna.",
-      li4: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-
-  },
-  {
-    head:" .SrFrontend Developer",
+    head: " .SrFrontend Developer",
     li1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     li2: "Ut pretium arcu et massa semper, id fringilla leo semper.",
     li3: "Sed quis justo ac magna.",
     li4: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-
-},
-{
-  head:" .SrFrontend Developer",
-  li1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  li2: "Ut pretium arcu et massa semper, id fringilla leo semper.",
-  li3: "Sed quis justo ac magna.",
-  li4: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-
-},
+  },
+  {
+    head: " Team Lead",
+    li1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    li2: "Ut pretium arcu et massa semper, id fringilla leo semper.",
+    li3: "Sed quis justo ac magna.",
+    li4: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    head: " .Full Stack Developer",
+    li1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    li2: "Ut pretium arcu et massa semper, id fringilla leo semper.",
+    li3: "Sed quis justo ac magna.",
+    li4: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
 ];
 
 export default function Portfolio() {
@@ -159,11 +157,23 @@ export default function Portfolio() {
       <div>
         <Experience></Experience>
       </div>
-    <div className="mt-24 mb-12">
-    {experience.map((experiences)=>{
-    return <Experience2 key={experiences.head} li1={experiences.li1} li2={experiences.li2} li3={experiences.li3} li4={experiences.li4} />
-  })}
-    </div>
+      <div className="mt-24 mb-24">
+        {experience.map((experiences) => {
+          return (
+            <Experience2
+              head={experiences.head}
+              li1={experiences.li1}
+              li2={experiences.li2}
+              li3={experiences.li3}
+              li4={experiences.li4}
+            />
+          );
+        })}
+      </div>
+      <div className="mt-24">
+        <Work></Work>
+        <Workscript></Workscript>
+      </div>
     </div>
   );
 }
