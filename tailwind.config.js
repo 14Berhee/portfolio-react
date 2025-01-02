5cd1c0a98f659865525a36851d13ba18544947bd
24LP4300 24LP4300 <24LP4300@admins-iMac.local>
2025-01-02 00:38:19 -0800

    deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
- "./components/**/*.{js,ts,jsx,tsx}",
+"./Components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
